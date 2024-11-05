5539bedb03acc05c99227bb5054b5bdc7f3ebc25
James Coliz <jcoliz@outlook.com>
2024-11-05 12:09:32 -0800

    feat(db): Add second row of data

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -1,1 +1,2 @@
-INSERT INTO `rolodex`.`contacts` (`id`, `name`, `age`, `email`) VALUES ('1', 'Test', '25', 'test@testing.com');
+INSERT INTO `rolodex`.`contacts` (`id`, `name`, `age`, `email`) VALUES ('1', 'Test', '11', 'test@testing.com');
+INSERT INTO `rolodex`.`contacts` (`id`, `name`, `age`, `email`) VALUES ('2', 'Test-2', '22', 'test-22@testing.com');
