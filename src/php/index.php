7e28d330e61c156b5509ccb6ab3ffeac1c25978f
James Coliz <jcoliz@outlook.com>
2024-11-05 10:11:37 -0800

    feat: Integrate php-fpm

---
+++ src/php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <title>My first PHP page</title>
+</head>
+
+<body>
+    <h1>PHP Details</h1>
+    <p><?php echo phpinfo(); ?></p>
+</body>
+
+</html>
